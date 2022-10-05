eea3efaaea772878d5d726fccca0764f919ab0aa
reza.jalaei <reza@figpii.com>
2022-10-06 00:35:31 +0300

    show dashboard when user exists

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,8 +1,8 @@
 import {useEffect, useState} from 'react';
 import {fetcher, useSessionContext, useStore} from '../lib/hooks';
-import {getSession} from '../../lib/auth';
+import {getSession} from '../lib/auth';
 
-import {getStoreAccessKey} from "@lib/dbs/mysql";
+import {getStoreAccessKey} from "../lib/dbs/mysql";
 
 let contextGlobal = "";
 
